306e81bd9e9e2c6543857aaf27c2ad4e88dfb700
Charles Burnham <felixtheslayer@gmail.com>
2016-04-17 17:25:42 -0500

    Framerate optimisations, more stuff in the level, sound effects

--- src/js/Game.js
+++ src/js/Game.js
@@ -10,8 +10,9 @@ LD35.Game = function () {
 LD35.Game.prototype = {
 	create: function () {
         this.game.time.advancedTiming = true;
-        this.stage.backgroundColor = '#222272';
+        this.stage.backgroundColor = '#191952';
 
+        this.frameCount = 0;
 
         //this.game.physics.startSystem(Phaser.Physics.P2JS);
         this.physics = this.game.physics.arcade;
@@ -27,12 +28,34 @@ LD35.Game.prototype = {
         this.tileset = this.bg.addTilesetImage('tiles', 'bg-tiles');
 
         var layer = this.bg.createLayer('Tile Layer 1');
+        this.game.world.remove(layer);
         this.spriteBatch = new Phaser.SpriteBatch(this.game);
         this.spriteBatch.add(layer);
+        layer.renderSettings.enableScrollDelta = true;
+
+        /*
+        var func = layer.renderFull;
+        var count = 0;
+        layer.renderFull = function () {
+            console.log('needing full render');
+            //count++;
+            //if (count % 8 === 0) {
+                func.call(this);
+            //}
+        };
+
+        
+        var func2 = layer.renderDeltaScroll;
+        layer.renderDeltaScroll = function (arg1, arg2) {
+            console.log('delta render');
+            func2.call(this, arg1, arg2);
+        };
+        */
+        
 
         this.game.world.setBounds(0, 0, this.bg.widthInPixels, this.bg.heightInPixels);
 
-        this.bg.createFromObjects('Object Layer 1', 'playerSpawn', 'player');
+        this.bg.createFromObjects('Object Layer 1', 'playerSpawn', 'player', 0);
         var players = this.game.world.filter(function (obj) {
             if (obj.hasOwnProperty('key') && obj.key === 'player') { 
                 return true;
@@ -40,12 +63,45 @@ LD35.Game.prototype = {
             return false; 
         });
         this.player = players.list[0];
+        this.player.inAir = false;
+        this.player.isAParachute = false;
+        this.player.canParachute = false;
+        this.player.jumpPushed = false;
         this.player.getPlayerBounds = function () {
             return new Phaser.Rectangle(this.x, this.y, this.width, this.height);
+        };
+        this.player.beAParachute = function () {
+            if (!this.canParachute) {
+                return;
+            }
+            this.isAParachute = true;
+            this.frame = 1;
+            this.body.velocity.y = 0;
+            this.body.gravity.setTo(0, -400);
+            this.body.maxVelocity.y = 600;
+            this.angle = 30;
+            this.tween = this.game.tweens.create(this).to({angle: -30}, 600, Phaser.Easing.Linear.None, false, 0, -1, true);
+            this.tween.start();
+
+            this.paraSound.play();
+        };
+        this.player.stopBeingAParachute = function () {
+            if (this.tween) {
+                this.tween.stop();
+                this.tween = null;
+                this.angle = 0;
+            }
+
+            this.jumpPushed = false;
+            this.isAParachute = false;
+            this.frame = 0;
+            this.body.gravity.setTo(0, 0);
+            this.body.maxVelocity.y = 5000;
         };
         this.player.anchor.setTo(0.5, 0.5);
 
         this.physics.enable(this.player);
+        this.player.body.maxVelocity.y = 5000;
         this.player.body.drag.setTo(2000, 0);
 
 
@@ -70,6 +126,16 @@ LD35.Game.prototype = {
         this.mapObjects.filter(function (obj) { return obj.name === "blockZone"; }).forEach(function (obj) {
             var group = new Phaser.Group(this.game);
             group.zoneRectangle = new Phaser.Rectangle(obj.x, obj.y, obj.width, obj.height);
+            if (obj.properties.hasOwnProperty('max-blocks')) {
+                group.maxBlocks = obj.properties['max-blocks'];
+            } else {
+                group.maxBlocks = 3;
+            }
+            if (obj.properties.hasOwnProperty('max-scale')) {
+                group.maxScale = obj.properties['max-scale'];
+            } else {
+                group.maxScale = 3.5;
+            }
             this.blockZones.add(group);
         }, this);
 
@@ -80,21 +146,49 @@ LD35.Game.prototype = {
             var g = this.game.add.graphics(obj.x, obj.y);
             g.beginFill(0x000000);
             g.drawRect(0, 0, obj.width, obj.height);
-            var emitter = this.game.add.emitter(obj.x + obj.width/2, obj.y + obj.height/2, 800);
-            emitter.gravity = -600;
-            emitter.minParticleSpeed.setTo(0, 3);
-            emitter.maxParticleSpeed.setTo(0, 44);
-            emitter.minParticleScale = 0.8;
-            emitter.minParticleScale = 1.4;
-            emitter.area = new Phaser.Rectangle(0, 0, obj.width, obj.height);
-            //emitter.makeParticles(['particle']);
-            emitter.particleClass = LD35.DeathParticle;
-            emitter.makeParticles();
 
-            emitter.flow(600, 1, 20, -1);
+            var emitterParent = {children: []};
+            var thickness = 5;
+            var rects = [
+                new Phaser.Rectangle(0, 0, obj.width, thickness),
+                new Phaser.Rectangle(0, 0, thickness, obj.height),
+                new Phaser.Rectangle(0, obj.height - thickness, obj.width, thickness),
+                new Phaser.Rectangle(obj.width - thickness, 0, thickness, obj.height),
+            ];
 
-            this.emitters.push(emitter);
-            this.deathZones.push(new Phaser.Rectangle(obj.x, obj.y, obj.width, obj.height));
+            rects.forEach(function (rect) {
+                var emitter = this.game.add.emitter(obj.x + rect.centerX, obj.y + rect.centerY, 200);
+                emitter.gravity = -600;
+                emitter.minParticleSpeed.setTo(0, 3);
+                emitter.maxParticleSpeed.setTo(0, 44);
+                emitter.minParticleScale = 0.8;
+                emitter.minParticleScale = 1.4;
+                emitter.area = rect;
+
+                emitter.particleClass = LD35.DeathParticle;
+                emitter.makeParticles();
+
+                emitter.flow(600, 1, 2, -1);
+
+                emitter.on = false;
+                emitterParent.children.push(emitter);
+            }, this);
+
+            emitterParent.turnOn = function () {
+                this.children.forEach(function (emitter) {
+                    emitter.on = true;
+                });
+            };
+            emitterParent.turnOff = function () {
+                this.children.forEach(function (emitter) {
+                    emitter.on = false;
+                });
+            };
+
+            var deathZone = new Phaser.Rectangle(obj.x, obj.y, obj.width, obj.height);
+            emitterParent.zone = deathZone;
+            this.emitters.push(emitterParent);
+            this.deathZones.push(deathZone);
         }, this);
 
         this.mapObjects.filter(function (obj) { return obj.name === "kill"; }).forEach(function (obj) {
@@ -102,6 +196,18 @@ LD35.Game.prototype = {
             g.beginFill(0x000000);
             g.drawRect(0, 0, obj.width, obj.height);
             this.deathZones.push(new Phaser.Rectangle(obj.x, obj.y, obj.width, obj.height));
+        }, this);
+
+        this.pickups = this.game.add.group();
+        this.mapObjects.filter(function (obj) { return obj.name === "pickup"; }).forEach(function (obj) {
+            var pickup = this.pickups.create(obj.x, obj.y, obj.type);
+            pickup.x -= pickup.width/2;
+            pickup.y -= pickup.height/2;
+            pickup.anchor.setTo(0.5, 0.5);
+            pickup.type = obj.type;
+
+            this.physics.enable(pickup);
+            pickup.body.allowGravity = false;
         }, this);
 
         this.controls = {
@@ -113,6 +219,23 @@ LD35.Game.prototype = {
             reset: this.game.input.keyboard.addKey(Phaser.Keyboard.R),
             pause: this.game.input.keyboard.addKey(Phaser.Keyboard.P),
         };
+
+        this.sfx = {};
+        this.sfx.death = this.game.add.audio('death');
+        this.sfx.pops = {sounds: []};
+        for (var sfx_i = 0; sfx_i < 6; sfx_i++) {
+            this.sfx.pops.sounds.push(this.game.add.audio('pop'));
+        }
+        this.sfx.pops.play = function () {
+            var availableSounds = this.sounds.filter(function (sound) { return !sound.isPlaying; });
+            if (availableSounds.length > 0) {
+                availableSounds[0].play();
+            }
+        };
+        this.sfx.jump = this.game.add.audio('jump');
+        this.sfx.para = this.game.add.audio('para');
+
+        this.player.paraSound = this.sfx.para;
 
         this.controls.reset.onDown.add(function () {this.resetGame();}, this);
         this.controls.pause.onDown.add(function () {this.togglePause();}, this);
@@ -129,6 +252,21 @@ LD35.Game.prototype = {
         if (this.gamePaused) {
             return;
         }
+        this.frameCount++;
+
+        if (this.frameCount % 20 === 0) {
+            this.emitters.forEach(function (emitter) {
+                if (!emitter.on) {
+                    if (emitter.zone.intersects(this.game.camera.view)) {
+                        emitter.turnOn();
+                    }
+                } else {
+                    if (!emitter.zone.intersects(this.game.camera.view)) {
+                        emitter.turnOff();
+                    }
+                }
+            }, this);
+        }
 
         if (this.player.alive) {
             this.blockZones.forEach(function (zone) {
@@ -138,6 +276,7 @@ LD35.Game.prototype = {
             //this.physics.collide(this.player, this.blockZones);
             //this.physics.collide(this.player, this.staticBlocks);
             this.physics.overlap(this.player, this.staticBlocks, null, this.separator.specialSeparate, this.separator);
+            this.physics.overlap(this.player, this.pickups, this.touchedPickup, null, this);
 
             var playerBounds = this.player.getPlayerBounds();
             this.deathZones.forEach(function (deathZone) {
@@ -147,13 +286,34 @@ LD35.Game.prototype = {
             }, this);
 
             if (this.controls.jump.isDown && this.player.body.touching.down) {
-                this.player.body.velocity.y = -300;
+                this.player.body.velocity.y = -340;
+                if (this.player.isAParachute) {
+                    this.player.stopBeingAParachute();
+                }
+                this.player.jumpPushed = true;
+                this.player.body.touching.down = false;
+                this.sfx.jump.play();
             }
 
             if (this.player.body.touching.down) {
+                if (this.player.inAir) {
+                    this.player.inAir = false;
+                    this.player.stopBeingAParachute();
+                }
                 this.player.body.drag.x = 2000;
             } else {
-                this.player.body.drag.x = 40;
+                if (!this.player.inAir) {
+                    this.player.inAir = true;
+                }
+                if (!this.player.isAParachute) {
+                    if (this.player.jumpPushed && !this.controls.jump.isDown) {
+                        this.player.jumpPushed = false;
+                        this.player.beAParachute();
+                    }
+                } else if (this.controls.down.isDown) {
+                    this.player.stopBeingAParachute();
+                }
+                this.player.body.drag.x = 90;
             }
 
             var movePressed = (this.controls.left.isDown || this.controls.right.isDown) && !(this.controls.left.isDown && this.controls.right.isDown);
@@ -162,6 +322,8 @@ LD35.Game.prototype = {
                 var sign = this.controls.left.isDown ? -1 : 1;
                 if (this.player.body.touching.down) {
                     targetVelocity = 300;
+                } else if (this.player.isAParachute) {
+                    targetVelocity = 200;
                 }
                 if (this.player.body.velocity.x * sign < targetVelocity) {
                     this.player.body.velocity.x += (targetVelocity * sign)/3;
@@ -170,15 +332,18 @@ LD35.Game.prototype = {
         }
 
         this.blockZones.forEach(function (zone) {
-            if (zone.total >= 3) {
+            if (zone.total >= zone.maxBlocks) {
                 return;
             }
             var bounds = zone.zoneRectangle;
             var randX = this.randomBetween(bounds.left, bounds.right);
             var randY = this.randomBetween(bounds.top, bounds.bottom);
+            if (this.game.camera.view.contains(randX, randY)) {
+                this.sfx.pops.play();
+            }
 
-            var randScaleX = this.randomBetween(0.8, 4);
-            var randScaleY = this.randomBetween(0.8, 4);
+            var randScaleX = this.randomBetween(0.8, zone.maxScale);
+            var randScaleY = this.randomBetween(0.8, zone.maxScale);
 
             var randomDeltaX = this.randomBetween(0.02, 0.005);
             var randomDeltaY = this.randomBetween(0.02, 0.005);
@@ -246,25 +411,37 @@ LD35.Game.prototype = {
         };
     },
 
+    touchedPickup: function (player, pickup) {
+        console.log(pickup.type);
+        if (pickup.type === 'parachute') {
+            player.canParachute = true;
+        }
+        pickup.kill();
+    },
+
     render: function () {
-        this.game.debug.text(this.game.time.fps, 10, 10, '#FF0000');
+        this.game.debug.text('fps: ' + this.game.time.fps, 10, 20, '#FF0000');
     },
 
     playerDeath: function () {
         this.player.alive = false;
         this.player.body.allowGravity = false;
         this.player.body.velocity.setTo(0, 0);
-        console.log('player dead');
+        if (this.player.tween) {
+            this.player.tween.stop();
+        }
         var black = this.game.add.sprite(0, 0, 'particle');
         black.width = this.game.width;
         black.height = this.game.height;
         black.alpha = 0;
         black.fixedToCamera = true;
-        var tween = this.game.tweens.create(black).to({alpha: 1}, 1200);
+        var tween = this.game.tweens.create(black).to({alpha: 1}, 1520);
         tween.onComplete.add(function () {
             this.resetGame();
         }, this);
         tween.start();
+
+        this.sfx.death.play();
     },
 
     resetGame: function () {
--- src/js/Preloader.js
+++ src/js/Preloader.js
@@ -10,13 +10,19 @@ LD35.Preloader = function () {
 
 LD35.Preloader.prototype = {
 	preload: function () {
-        this.game.load.image('player', '/assets/img/player.png');
+        this.game.load.spritesheet('player', '/assets/img/player.png', 20, 26, -1, 0, 1);
         this.game.load.image('block', '/assets/img/block.png');
         this.game.load.image('block-green', '/assets/img/block_green.png');
         this.game.load.image('particle', '/assets/img/particle.png');
+        this.game.load.image('parachute', '/assets/img/parachute.png');
 
         this.game.load.image('bg-tiles', '/assets/img/bg_tiles.png');
         this.game.load.tilemap('bg-map', '/assets/map/bg.json', null, Phaser.Tilemap.TILED_JSON);
+
+        this.game.load.audio('death', ['assets/sfx/death.mp3', 'assets/sfx/death.ogg']);
+        this.game.load.audio('pop', ['assets/sfx/pop.mp3', 'assets/sfx/pop.ogg']);
+        this.game.load.audio('jump', ['assets/sfx/jump.mp3', 'assets/sfx/jump.ogg']);
+        this.game.load.audio('para', ['assets/sfx/para.mp3', 'assets/sfx/para.ogg']);
 	},
 
 	create: function () {
