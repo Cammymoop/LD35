e59378630c37696c95fc682fc6e48a9c1e2c0c47
Charles Burnham <felixtheslayer@gmail.com>
2016-04-17 22:44:24 -0500

    update dist

--- dist/js/Boot.js
+++ dist/js/Boot.js
@@ -5,13 +5,11 @@ var LD35 = scope.LD35;
 var Phaser = scope.Phaser;
 
 LD35.Boot = function () {
-    "use strict";
 
 };
 
 LD35.Boot.prototype = {
 	create: function () {
-        "use strict";
 		this.game.input.maxPointers = 1;
 
 		this.game.stage.disableVisibilityChange = true;
@@ -25,8 +23,8 @@ LD35.Boot.prototype = {
 		    this.game.stage.scaleMode = Phaser.StageScaleMode.SHOW_ALL;
 		    this.game.stage.scale.minWidth = 480;
 		    this.game.stage.scale.minHeight = 260;
-		    this.game.stage.scale.maxWidth = 1024;
-		    this.game.stage.scale.maxHeight = 768;
+		    this.game.stage.scale.maxWidth = 800;
+		    this.game.stage.scale.maxHeight = 600;
 		    this.game.stage.scale.forceLandscape = true;
 		    this.game.stage.scale.pageAlignHorizontally = true;
 		    this.game.stage.scale.setScreenSize(true);
